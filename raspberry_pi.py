27420b54130351501aa7f34e439580e266d8e221
Louis-Philippe Simard <64223102+Louispoire@users.noreply.github.com>
2021-12-09 20:41:26 -0500

    Update raspberry_pi.py
    Added some new functionality

--- raspberry_pi.py
+++ raspberry_pi.py
@@ -2,60 +2,59 @@ import adafruit_dht
 import board
 from datetime import date
 import time
-from TemperatureValidator import *
+from WebScraper import WeatherScraper
 import requests
 
 # Setting up the pin
 dht_device = adafruit_dht.DHT22(board.D4, False)
-
-
+scraper = WeatherScraper()
 # Use to calculate temperature and humidity at request
 def CalculateCurrentTemperature():
     humidity = dht_device.temperature
     temperature = dht_device.humidity
     return temperature, humidity
 
+def SendRequest(p):
+    try:
+        print("Commencing temperature transfer...")
+        #meteomedia = scraper.get_current_temp_from_meteomedia()
+        #weathercom = scraper.get_current_temp_from_weathercom()
+        temp = { 'temp': 22, 'tempWeatherCom': 20, 'tempMeteoMedia': 20 }
+        post_request = requests.post('https://weatheranalyser.pythonanywhere.com/', data=temp)
+        print(post_request.text)
+        
+        if post_request.status_code == requests.codes.ok:
+            return "OK"
+        else:
+            return "Bad request"
+    except Exception:
+        return "error"
+    
 
-meteomedia = 25
-weather = 26
 
 while True:
-    test_file = open('test.txt', 'a')
     try:
+        print("go")
         # Temp/Humi
         humidity = dht_device.humidity
         temperature = dht_device.temperature
 
-        # day/month/time
-        this_day = date.today()
-        this_month = date.month()
-        t = time.localtime()
-        this.time = time.strftime("%H:%M", t)
-
-        print(this_day + " " + this_month + " " + this.time)
-
-        results = precision_calculator(meteomedia, weather, temperature)
-        status = results[0]
-        precision = results[1]
-        print("Temperature: " + str(temperature) + " | Humidity: " + str(humidity))
-        print(status + " " + str(precision) + "%")
-        test_file.write(
-            "Temperature: " + str(temperature) + " | Humidity: " + str(humidity) + " | " + status + " " + str(
-                precision) + "%\n")
-
-        sendRequest(temperature)
-        time.sleep(5)
+        # results = precision_calculator(meteomedia, weather, temperature)
+        # status = results[0]
+        # precision = results[1]
+        # print(status + " " + str(precision) + "%")
+        if temperature == None:
+            continue
+        else:
+            print("Temperature: " + str(temperature) + " | Humidity: " + str(humidity))
+            result = SendRequest(temperature)
+            if result == "error":
+            print("Error. Request could not be sent")
+            elif result == "Bad request":
+            print("Request was sent but failed")
+            elif result == "OK":
+            print("Request was sucessfully sent!")
+            time.sleep(5)
     except RuntimeError:
         print("Runtime error has occured. This is generally due to the sensor failing")
         continue
-
-
-def SendRequest(temperature):
-    temp = {'temp': temperature}
-    post_request = requests.post('uri', data=temp)
-
-    if post_request.status_code = requests.codes.ok:
-        print("nice")
-
-
-
